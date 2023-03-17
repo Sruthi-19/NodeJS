8644dc7e98e15d6b65498c39ce5f6584af707a85
Sruthi Ranganathan <sruthi19ranga@gmail.com>
2023-03-17 21:54:42 +0530

    Update

--- product_backend/app.js
+++ product_backend/app.js
@@ -12,13 +12,24 @@ app.get("/products", (req, res) => {
   let pdt = productModel.find().then((pdt) => res.status(200).json(pdt));
 });
 
-//Filter products based on category, rating and price range if given.
+//Post new product
+app.post("/addProduct", (req, res) => {
+  const newProduct = new productModel(req.body);
+  newProduct.save().then((result) => {
+    res.status(200).json(result);
+  });
+});
+
+//Filter products based on category, rating and price range if given. The page and limit value can also be entered
 app.get("/product/filter", (req, res) => {
   let filter = {};
   let minPrice = 0;
   let maxPrice = 10000;
+  let limitValue = 4;
+  let skipValue = 0;
+
   if (req.query.category) {
-    filter["category"] = req.query.category;
+    filter.category = req.query.category;
   }
   if (req.query.rating) {
     filter.rating = req.query.rating;
@@ -29,23 +40,6 @@ app.get("/product/filter", (req, res) => {
   if (req.query.max) {
     maxPrice = req.query.max;
   }
-  console.log(filter);
-  let pdt = productModel
-    .find({
-      $and: [
-        filter,
-        {
-          $and: [{ price: { $gte: minPrice } }, { price: { $lte: maxPrice } }],
-        },
-      ],
-    })
-    .then((pdt) => res.status(200).json(pdt));
-});
-
-//Get page wise products in entered order and limit number of products to be displayed in that page
-app.get("/product/page", (req, res) => {
-  let limitValue = 4;
-  let skipValue = 0;
 
   if (req.query.limit && req.query.page) {
     limitValue = req.query.limit;
@@ -57,19 +51,39 @@ app.get("/product/page", (req, res) => {
   }
 
   let pdt = productModel
-    .find({})
+    .find({
+      $and: [
+        filter,
+        {
+          $and: [{ price: { $gte: minPrice } }, { price: { $lte: maxPrice } }],
+        },
+      ],
+    })
     .skip(skipValue)
     .limit(limitValue)
     .then((pdt) => res.status(200).json(pdt));
 });
 
-//Post new pro
-app.post("/addProduct", (req, res) => {
-  const newProduct = new productModel(req.body);
-  newProduct.save().then((result) => {
-    res.status(200).json(result);
-  });
-});
+//Get page wise products in entered order and limit number of products to be displayed in that page
+// app.get("/product/page", (req, res) => {
+//   let limitValue = 4;
+//   let skipValue = 0;
+
+//   if (req.query.limit && req.query.page) {
+//     limitValue = req.query.limit;
+//     skipValue = (req.query.page - 1) * limitValue;
+//   } else if (req.query.page && !req.query.limit) {
+//     skipValue = (req.query.page - 1) * limitValue;
+//   } else if (req.query.limit && !req.query.page) {
+//     limitValue = req.query.limit;
+//   }
+
+//   let pdt = productModel
+//     .find({})
+//     .skip(skipValue)
+//     .limit(limitValue)
+//     .then((pdt) => res.status(200).json(pdt));
+// });
 
 //Get products between price range
 // app.get("/product/price", (req, res) => {
